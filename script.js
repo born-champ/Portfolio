4f845f1464ca0aa2b5d60209ec94b08eda1a448e
born-champ <ashudubey8055@gmail.com>
2021-06-14 14:37:42 +0530

    skills and contact pages added

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 $(document).ready(function(){
 	$(window).ready(function(){
-		if(this.scrollY>20){
+		if(this.scrollY > 20){
 			$('.navbar').addclass("sticky");
 		}else{
 			$('.navbar').removeclass("sticky");
 
 		}
+		if (this.scroll > 500) {
+			$('.scroll-up-btn').addclass("show");
+
+		}else{
+			$('.scroll-up-btn').removeclass("show");
+
+		}
 	});
+	$('.scroll-up-btn').click(function(){
+		$('html').animate({scrollTop: 0});
+	});
+
 	$('.menu-btn').click(function(){
 		$('.navbar .menu').toggleClass("active");
 		$('.menu-btn i').toggleClass("active");
